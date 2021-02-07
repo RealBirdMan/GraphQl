39a11bafc452461590ff06c919016ddadab011ac
Julian Vogel <webdev-vogel@outlook.de>
2021-02-07 18:56:43 +0100

    adding mongoose

--- server/app.js
+++ server/app.js
@@ -1,13 +1,31 @@
-const express = require('express');
-const {graphqlHTTP} = require("express-graphql");
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const mongoose = require("mongoose");
 
-const schema = require('./schema/schema')
-
+const schema = require("./schema/schema");
 
 const app = express();
 
-app.use("/graphql", graphqlHTTP({schema}));
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
 
-app.listen(8080, () => {
-    console.log("Server running")
-})
+const PORT = process.env.PORT || 8080;
+
+mongoose
+  .connect("connection string here", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    app.listen(PORT);
+    console.log("db and server are running");
+  })
+  .catch(err => {
+    console.error(err);
+  });
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,37 +1,79 @@
 const graphql = require("graphql");
 const _ = require("lodash");
 
-const {GraphQLObjectType, GraphQLString, GraphQLSchema} = graphql;
-
-const DUMMY_DATA = [
-    {name: "Peter Brown", genere: "Fantasy", id:"1"},
-    {name: "Peter Yellow", genere: "Horror", id:"2"},
-    {name: "Brown Peter", genere: "History", id:"3"}
-]
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLID,
+  GraphQLInt,
+  GraphQLList,
+} = graphql;
 
 const BookType = new GraphQLObjectType({
-    name: "Book",
-    fields: () => ({
-        id: {type: GraphQLString},
-        name: {type: GraphQLString},
-        genere: {type: GraphQLString}
-    })
+  name: "Book",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    genere: { type: GraphQLString },
+    author: {
+      type: AuthorType,
+      resolve(parent, args) {
+        //return _.find(DUMMY_AUTHORS, { id: parent.authorId });
+      },
+    },
+  }),
+});
+
+const AuthorType = new GraphQLObjectType({
+  name: "Author",
+  fields: () => ({
+    id: { type: GraphQLID },
+    age: { type: GraphQLInt },
+    name: { type: GraphQLString },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args) {
+        //return _.filter(DUMMY_BOOKS, { authorId: parent.id });
+      },
+    },
+  }),
 });
 
 const RootQuery = new GraphQLObjectType({
-    name: "RootQueryType",
-    fields: {
-        book: {
-            type: BookType,
-            args: {id:{type: GraphQLString}},
-            resolve(parent, args){
-                //code to get data from db
-               return DUMMY_DATA.find(books, {id: args.id});
-            }
-        }
-    }
+  name: "RootQueryType",
+  fields: {
+    book: {
+      type: BookType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        //code to get data from db
+        // return _.find(DUMMY_BOOKS, { id: args.id });
+      },
+    },
+    author: {
+      type: AuthorType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        //code to get data from db
+        //return _.find(DUMMY_AUTHORS, { id: args.id });
+      },
+    },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args) {
+        //return DUMMY_BOOKS;
+      },
+    },
+    authors: {
+      type: new GraphQLList(AuthorType),
+      resolve(parent, args) {
+        // return DUMMY_AUTHORS;
+      },
+    },
+  },
 });
 
 module.exports = new GraphQLSchema({
-    query: RootQuery
-})
+  query: RootQuery,
+});
